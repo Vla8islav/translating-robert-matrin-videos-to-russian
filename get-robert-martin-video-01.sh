0ed51b3d617be34c934f1df7ee30da75f6cd48dc
Vladislav Pilipenko <pilipenko@banki.ru>
2017-02-17 23:04:21 +0300

    Initial commit. Uses youtube-dl. This package avaliable in most Linux distributions.

---
+++ get-robert-martin-video-01.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#youtube-dl https://www.youtube.com/watch?v=TMuno5RZNeE
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mv $DIR/Bob\ Martin\ \ \ SOLID\ Principles\ of\ Object\ Oriented\ and\ Agile\ *.mp4 $DIR/Bob\ Martin\ \ \ SOLID\ Principles\ of\ Object\ Oriented\ and\ Agile\ Design.mp4
